29e5600a50fb8588c09f9e498229c09600c451be
newmediapilot <marcin.a.zajkowski@gmail.com>
2024-11-12 03:18:02 -0500

    improve timer

--- src/countDown.js
+++ src/countDown.js
@@ -5,9 +5,18 @@ function countDown(interval) {
 
     const countdownInterval = setInterval(() => {
         remainingTime -= 100;
+
+        // Calculate the percentage complete
         const percentComplete = Math.min(100, ((interval - remainingTime) / interval) * 100).toFixed(1);
 
-        process.stdout.write(`\r⏳ Progress: ${percentComplete}% - Time left: ${remainingTime}ms `);
+        // Calculate HH:MM:SS
+        const hours = String(Math.floor(remainingTime / 3600000)).padStart(2, '0');
+        const minutes = String(Math.floor((remainingTime % 3600000) / 60000)).padStart(2, '0');
+        const seconds = String(Math.floor((remainingTime % 60000) / 1000)).padStart(2, '0');
+        const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+        // Display countdown progress
+        process.stdout.write(`\r⏳ Progress: ${percentComplete}% - Time left: ${remainingTime}ms || ${formattedTime} || Time until next tweet`);
 
         if (remainingTime <= 0) {
             clearInterval(countdownInterval);
